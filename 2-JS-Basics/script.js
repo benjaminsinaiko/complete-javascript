31fe53349107f5cd3a93367e360ee216a2cc63ba
benjaminsinaiko <ben.jamin@outlook.com>
2018-03-14 12:42:21 -0500

    Section 2, operators

--- 2-JS-Basics/script.js
+++ 2-JS-Basics/script.js
@@ -14,54 +14,77 @@ console.log(fullAge);
 */
 
 //Lecture: variables 2
-var name = "John";
-var age = 26;
+// var name = "John";
+// var age = 26;
 
-// console.log(name + age);
-// console.log(age + age);
+// // console.log(name + age);
+// // console.log(age + age);
 
-var job, isMarried;
+// var job, isMarried;
 
-// console.log(job);
+// // console.log(job);
 
-job = "teacher";
-isMarried = false;
+// job = "teacher";
+// isMarried = false;
 
-console.log(
-  name +
-    " is a " +
-    age +
-    " year old " +
-    job +
-    ". Is he married? " +
-    isMarried +
-    "."
-);
+// console.log(
+//   name +
+//     " is a " +
+//     age +
+//     " year old " +
+//     job +
+//     ". Is he married? " +
+//     isMarried +
+//     "."
+// );
 
-age = "thirty six";
-job = "driver";
+// age = "thirty six";
+// job = "driver";
 
-console.log(
-  name +
-    " is a " +
-    age +
-    " year old " +
-    job +
-    ". Is he married? " +
-    isMarried +
-    "."
-);
+// console.log(
+//   name +
+//     " is a " +
+//     age +
+//     " year old " +
+//     job +
+//     ". Is he married? " +
+//     isMarried +
+//     "."
+// );
 
-var lastName = prompt("What is the last name?");
-console.log(lastName);
+// var lastName = prompt("What is the last name?");
+// console.log(lastName);
 
-alert(
-  name +
-    " is a " +
-    age +
-    " year old " +
-    job +
-    ". Is he married? " +
-    isMarried +
-    "."
-);
+// alert(
+//   name +
+//     " is a " +
+//     age +
+//     " year old " +
+//     job +
+//     ". Is he married? " +
+//     isMarried +
+//     "."
+// );
+
+// Lecture operators
+var now = 2018;
+var birthYear = now - 26;
+
+birthYear = now - 26 * 2;
+//2018 - 52
+//1966
+
+console.log(birthYear);
+
+var ageJohn = 30;
+var ageMark = 30;
+
+ageJohn = ageMark = (3 + 5) * 4 - 6;
+//ageJohn = ageMark = 26
+//ageJohn = 26
+
+ageJohn++;
+ageMark *= 2;
+
+console.log(ageJohn);
+console.log(ageMark);
