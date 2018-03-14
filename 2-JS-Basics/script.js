f5725fa29b50922dbea4bf1a69d3627f06b22606
benjaminsinaiko <ben.jamin@outlook.com>
2018-03-14 12:48:30 -0500

    Section 2, if/else statements

--- 2-JS-Basics/script.js
+++ 2-JS-Basics/script.js
@@ -13,59 +13,66 @@ var fullAge = true;
 console.log(fullAge);
 */
 
-//Lecture: variables 2
-// var name = "John";
-// var age = 26;
+/*
 
-// // console.log(name + age);
-// // console.log(age + age);
+////////////////////////////////////////
+Lecture: variables 2
+var name = "John";
+var age = 26;
 
-// var job, isMarried;
+// console.log(name + age);
+// console.log(age + age);
 
-// // console.log(job);
+var job, isMarried;
 
-// job = "teacher";
-// isMarried = false;
+// console.log(job);
 
-// console.log(
-//   name +
-//     " is a " +
-//     age +
-//     " year old " +
-//     job +
-//     ". Is he married? " +
-//     isMarried +
-//     "."
-// );
+job = "teacher";
+isMarried = false;
 
-// age = "thirty six";
-// job = "driver";
+console.log(
+  name +
+    " is a " +
+    age +
+    " year old " +
+    job +
+    ". Is he married? " +
+    isMarried +
+    "."
+);
 
-// console.log(
-//   name +
-//     " is a " +
-//     age +
-//     " year old " +
-//     job +
-//     ". Is he married? " +
-//     isMarried +
-//     "."
-// );
+age = "thirty six";
+job = "driver";
 
-// var lastName = prompt("What is the last name?");
-// console.log(lastName);
+console.log(
+  name +
+    " is a " +
+    age +
+    " year old " +
+    job +
+    ". Is he married? " +
+    isMarried +
+    "."
+);
 
-// alert(
-//   name +
-//     " is a " +
-//     age +
-//     " year old " +
-//     job +
-//     ". Is he married? " +
-//     isMarried +
-//     "."
-// );
+var lastName = prompt("What is the last name?");
+console.log(lastName);
 
+alert(
+  name +
+    " is a " +
+    age +
+    " year old " +
+    job +
+    ". Is he married? " +
+    isMarried +
+    "."
+);
+*/
+
+/*
+
+////////////////////////////////////////
 // Lecture operators
 var now = 2018;
 var birthYear = now - 26;
@@ -88,3 +95,29 @@ ageMark *= 2;
 
 console.log(ageJohn);
 console.log(ageMark);
+*/
+
+////////////////////////////////////////
+//Lecture: if/else statements
+
+var name = "John";
+var age = 26;
+var isMarried = "yes";
+
+if (isMarried === "yes") {
+  console.log(name + " is married!");
+} else {
+  console.log(name + " will hopefully marry soon :)");
+}
+
+isMarried = false;
+
+if (isMarried) {
+  console.log("YES!");
+} else {
+  console.log("NO!");
+}
+
+if (23 === "23") {
+  console.log("Something to print...");
+}
