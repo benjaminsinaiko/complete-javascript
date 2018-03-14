a658030e2a87fbb989cc314a151012ea574bc886
benjaminsinaiko <ben.jamin@outlook.com>
2018-03-14 14:21:06 -0500

    Section 2, objects

--- 2-JS-Basics/script.js
+++ 2-JS-Basics/script.js
@@ -253,7 +253,7 @@ var x = 3;
 
 ////////////////////////////////////////
 //Lecture: arrays
-
+/*
 var names = ["John", "Jane", "Mark"];
 var years = new Array(1990, 1969, 1948);
 
@@ -272,3 +272,35 @@ console.log(john);
 if (john.indexOf("teacher") === -1) {
   console.log("John is not a teacher");
 }
+*/
+
+////////////////////////////////////////
+//Lecture: objects
+
+var john = {
+  name: "John",
+  lastName: "Smith",
+  yearOfBirth: 1990,
+  job: "teacher",
+  isMarried: false
+};
+
+console.log(john.lastName);
+console.log(john["lastName"]);
+
+var xyz = "job";
+console.log(john[xyz]);
+
+john.lastName = "Miller";
+john["job"] = "programmer";
+
+console.log(john);
+
+var jane = new Object();
+jane.name = "jane";
+jane.lastName = "Smith";
+jane["yearOfBirth"] = 1969;
+jane["job"] = "retired";
+jane["isMarried"] = true;
+
+console.log(jane);
