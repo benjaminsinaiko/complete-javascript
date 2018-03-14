4cb0b2d9eb3a3188542295c64d831a16aacca1ed
benjaminsinaiko <ben.jamin@outlook.com>
2018-03-14 12:56:42 -0500

    Section 2, boolean logic and switch

--- 2-JS-Basics/script.js
+++ 2-JS-Basics/script.js
@@ -100,6 +100,7 @@ console.log(ageMark);
 ////////////////////////////////////////
 //Lecture: if/else statements
 
+/*
 var name = "John";
 var age = 26;
 var isMarried = "yes";
@@ -120,4 +121,34 @@ if (isMarried) {
 
 if (23 === "23") {
   console.log("Something to print...");
+}
+*/
+
+////////////////////////////////////////
+//Lecture: boolean logic and switch
+
+var age = 31;
+
+if (age < 20) {
+  console.log("John is a teenager");
+} else if (age >= 20 && age < 30) {
+  console.log("John is a young man.");
+} else {
+  console.log("John is a man.");
+}
+
+var job = prompt("What does John do?");
+
+switch (job) {
+  case "teacher":
+    console.log("John teaches kids");
+    break;
+  case "driver":
+    console.log("John drives a cab in Lisbon");
+    break;
+  case "cop":
+    console.log("John helps fight crime");
+    break;
+  default:
+    console.log("John does something else");
 }
