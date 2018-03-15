95701099faf7a6377dd7a0010e1b4243d5eb8b42
benjaminsinaiko <ben.jamin@outlook.com>
2018-03-15 14:11:27 -0500

    Add player switch and score reset after rolling 1

--- 4-DOM-Pig-Game/app.js
+++ 4-DOM-Pig-Game/app.js
@@ -34,4 +34,24 @@ document.querySelector(".btn-roll").addEventListener("click", function() {
   diceDOM.style.display = 'block';
   diceDOM.src = 'dice-' + dice + '.png';
 
+  // 3. Update the round score IF the rolled number ws not a 1
+  if (dice !== 1) {
+    //Add score
+    roundScore += dice;
+    document.querySelector('#current-' + activePlayer).textContent = roundScore;
+  } else {
+    //Next player
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    roundScore = 0;
+
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+
+    document.querySelector('.dice').style.display = 'none';
+
+  }
+
 });
