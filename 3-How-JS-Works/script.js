561b8a45c8673e7b2577e117b24fa118fb5f39a6
benjaminsinaiko <ben.jamin@outlook.com>
2018-03-15 01:12:03 -0500

    Section 3, the this keyword

--- 3-How-JS-Works/script.js
+++ 3-How-JS-Works/script.js
@@ -48,6 +48,7 @@ function first() {
 
 // Example to show the differece between execution stack and scope chain
 
+/*
 var a = "Hello!";
 first();
 
@@ -65,4 +66,40 @@ function third() {
   var d = "John";
   console.log(a + d);
 }
+*/
 
+///////////////////////////////////////
+// Lecture: The this keyword
+
+// console.log(this);
+
+calculateAge(1985);
+
+function calculateAge(year) {
+  console.log(2018 - year);
+  console.log(this);
+}
+
+var john = {
+  name: "John",
+  yearOfBirth: 1990,
+  calculateAge: function() {
+    console.log(this);
+    console.log(2018 - this.yearOfBirth);
+
+    function innerFunction() {
+      console.log(this);
+    }
+    innerFunction();
+  }
+};
+
+john.calculateAge();
+
+var mike = {
+  name: "Mike",
+  yearOfBirth: 1984
+};
+
+mike.calculateAge = john.calculateAge;
+mike.calculateAge();
