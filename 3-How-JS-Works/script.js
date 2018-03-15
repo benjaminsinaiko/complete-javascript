a6c77036d59987d7186b0a15af2b73ae6f7409bc
benjaminsinaiko <ben.jamin@outlook.com>
2018-03-15 00:58:43 -0500

    Section 3, scoping

--- 3-How-JS-Works/script.js
+++ 3-How-JS-Works/script.js
@@ -1,16 +1,16 @@
 ///////////////////////////////////////
 // Lecture: Hoisting
-
+/*
 calculateAge(1965);
 
 function calculateAge(year) {
-    console.log(2018 - year);
+  console.log(2018 - year);
 }
 
 // retirement(1965);
 
 var retirement = function(year) {
-    console.log(65 - (2016 - year));
+  console.log(65 - (2016 - year));
 };
 
 // variables
@@ -20,52 +20,49 @@ var age = 23;
 console.log(age);
 
 function foo() {
-    console.log(age);
-    var age = 65;
-    console.log(age);
+  console.log(age);
+  var age = 65;
+  console.log(age);
 }
 foo();
 console.log(age);
+*/
 
 ///////////////////////////////////////
 // Lecture: Scoping
 
-// First scoping example
-
 /*
-var a = 'Hello!';
+var a = "Hello!";
 first();
 
 function first() {
-    var b = 'Hi!';
-    second();
+  var b = "Hi!";
+  second();
 
-    function second() {
-        var c = 'Hey!';
-        console.log(a + b + c);
-    }
+  function second() {
+    var c = "Hey!";
+    console.log(a + b + c);
+  }
 }
 */
 
 // Example to show the differece between execution stack and scope chain
 
-// var a = "Hello!";
-// first();
+var a = "Hello!";
+first();
 
-// function first() {
-//     var b = "Hi!";
-//     second();
+function first() {
+  var b = "Hi!";
+  second();
 
-//     function second() {
-//         var c = "Hey!";
-//         third();
-//     }
-// }
+  function second() {
+    var c = "Hey!";
+    third();
+  }
+}
 
-// function third() {
-//     var d = "John";
-//     console.log(a + b + c + d);
-// }
+function third() {
+  var d = "John";
+  console.log(a + d);
+}
 
-///////////////////////////////////////
-// Lecture: The this keyword
