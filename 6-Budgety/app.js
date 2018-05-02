a1189e953f333e293aac4d2e7f6417ebe60cd4e8
benjaminsinaiko <ben.jamin@outlook.com>
2018-05-02 13:24:39 -0500

    Create DOMstrings object in UI controller, share with other modules

--- 6-Budgety/app.js
+++ 6-Budgety/app.js
@@ -5,19 +5,29 @@ var budgetController = (function() {
 
 // UI CONTROLLER
 var UIController = (function() {
+  var DOMstrings = {
+    inputType: '.add__type',
+    inputDescription: '.add__description',
+    inputValue: '.add__value',
+    inputBtn: '.add__btn'
+  };
   return {
     getInput: function() {
       return {
-        type: document.querySelector('.add__type').value, // Will be either inc or exp
-        description: document.querySelector('.add__description').value,
-        value: document.querySelector('.add__value').value
+        type: document.querySelector(DOMstrings.inputType).value, // Will be either inc or exp
+        description: document.querySelector(DOMstrings.inputDescription).value,
+        value: document.querySelector(DOMstrings.inputValue).value
       };
+    },
+    getDOMstrings: function() {
+      return DOMstrings;
     }
   };
 })();
 
 // GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
+  var DOM = UICtrl.getDOMstrings();
   var ctrlAddItem = function() {
     // 1. Get the input data
     var input = UICtrl.getInput();
@@ -28,7 +38,7 @@ var controller = (function(budgetCtrl, UICtrl) {
     // 5. Display the budget on the UI
   };
 
-  document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+  document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
 
   document.addEventListener('keypress', function(event) {
     if (event.keyCode === 13 || event.which === 13) {
