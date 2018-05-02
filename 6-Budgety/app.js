438c73738314adf9721261b4fc4b3da9983a56be
benjaminsinaiko <ben.jamin@outlook.com>
2018-05-02 12:33:16 -0500

    Add to-do list in global controller

--- 6-Budgety/app.js
+++ 6-Budgety/app.js
@@ -1,25 +1,21 @@
+// BUDGET CONTROLLER
 var budgetController = (function() {
-  var x = 23;
-  var add = function(a) {
-    return x + a;
-  };
-
-  return {
-    publicTest: function(b) {
-      return add(b);
-    }
-  };
+  // Some code
 })();
 
+// UI CONTROLLER
 var UIController = (function() {
   // Some code
 })();
 
+// GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
-  var z = budgetCtrl.publicTest(5);
-  return {
-    anotherPublic: function() {
-      console.log(z);
-    }
-  };
-})(budgetController, UIController);
+  document.querySelector('.add__btn').addEventListener('click', function() {
+    // 1. Get the input data
+    // 2. Add teh item to the budget controller
+    // 3. Add the item to the UI
+    // 4. Calculate the budget
+    // 5. Display the budget on the UI
+  });
+
+
