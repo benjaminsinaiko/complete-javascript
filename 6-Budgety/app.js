96bafdbafa22b3fb38b74a3baf451141d75f49a6
benjaminsinaiko <ben.jamin@outlook.com>
2018-05-02 23:53:58 -0500

    Add percentage sign in UICtrl,  and clear labels on init

--- 6-Budgety/app.js
+++ 6-Budgety/app.js
@@ -156,8 +156,13 @@ var UIController = (function() {
       document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc;
       document.querySelector(DOMstrings.expensesLabel).textContent =
         obj.totalExp;
-      document.querySelector(DOMstrings.percentageLabel).textContent =
-        obj.percentage;
+
+      if (obj.percentage > 0) {
+        document.querySelector(DOMstrings.percentageLabel).textContent =
+          obj.percentage + '%';
+      } else {
+        document.querySelector(DOMstrings.percentageLabel).textContent = '---';
+      }
     },
 
     getDOMstrings: function() {
@@ -219,6 +224,12 @@ var controller = (function(budgetCtrl, UICtrl) {
   return {
     init: function() {
       console.log('Application has started.');
+      UICtrl.displayBudget({
+        budget: 0,
+        totalInc: 0,
+        totalExp: 0,
+        percentage: -1
+      });
       setupEventListeners();
     }
   };
