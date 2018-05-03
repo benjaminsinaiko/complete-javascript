95bcf56c507da836efcb640a36e3764ecc799444
benjaminsinaiko <ben.jamin@outlook.com>
2018-05-02 23:47:25 -0500

    Add DOMstrings for UI budget labels, and displaybudget method UIController

--- 6-Budgety/app.js
+++ 6-Budgety/app.js
@@ -97,7 +97,11 @@ var UIController = (function() {
     inputValue: '.add__value',
     inputBtn: '.add__btn',
     incomeContainer: '.income__list',
-    expensesContainer: '.expenses__list'
+    expensesContainer: '.expenses__list',
+    budgetLabel: '.budget__value',
+    incomeLabel: '.budget__income--value',
+    expensesLabel: '.budget__expenses--value',
+    percentageLabel: '.budget__expenses--percentage'
   };
   return {
     getInput: function() {
@@ -147,6 +151,15 @@ var UIController = (function() {
       fieldsArr[0].focus();
     },
 
+    displayBudget: function(obj) {
+      document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
+      document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc;
+      document.querySelector(DOMstrings.expensesLabel).textContent =
+        obj.totalExp;
+      document.querySelector(DOMstrings.percentageLabel).textContent =
+        obj.percentage;
+    },
+
     getDOMstrings: function() {
       return DOMstrings;
     }
@@ -175,7 +188,7 @@ var controller = (function(budgetCtrl, UICtrl) {
     var budget = budgetCtrl.getBudget();
 
     // 3. Display the budget on the UI
-    console.log(budget);
+    UICtrl.displayBudget(budget);
   };
 
   var ctrlAddItem = function() {
