8d8c5586a43d7bb9bec23f372b64f5f3aa1dccf0
benjaminsinaiko <ben.jamin@outlook.com>
2018-05-03 23:36:23 -0500

    Variables, let vs const, simple example

---
+++ 7-EcmaScript2015/script.js
@@ -0,0 +1,15 @@
+Lecture: let and const
+
+// ES5
+var name5 = 'Jane Smith';
+var age5 = 23;
+name5 = 'Jane Miller';
+console.log(name5);
+
+// ES6
+const name6 = 'Jane Smith';
+let age = 23;
+name6 = 'Jane Miller';
+console.log(name6);
+
+
