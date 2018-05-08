6706648db55491bc648dc38440aa4f3c0901c1c0
benjaminsinaiko <ben.jamin@outlook.com>
2018-05-08 16:26:00 -0500

    ES6, maps - set, get, has, size, clear

--- 7-EcmaScript2015/script.js
+++ 7-EcmaScript2015/script.js
@@ -414,3 +414,29 @@ isFullAge6(1990, 1999, 1965);
 // console.log(john);
 // const emily = new SmithPerson('Emily', 1986, 'Torres', 'spanish');
 // console.log(emily);
+
+////////////////////////////////////////
+// Lecture: Maps
+
+const question = new Map();
+question.set(
+  'question',
+  'What is the official name of the latest major JavaScript version?'
+);
+question.set(1, 'ES5');
+question.set(2, 'ES6');
+question.set(3, 'ES2015');
+question.set(4, 'ES7');
+question.set('correct', 3);
+question.set(true, 'Correct answer');
+question.set(false, 'Wrong, please try again');
+
+console.log(question.get('question'));
+console.log(question.size);
+
+if (question.has(4)) {
+  // question.delete(4);
+  console.log('Answer 4 is here');
+}
+
+// question.clear();
