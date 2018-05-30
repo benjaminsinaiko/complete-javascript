a5b820d3bbf30fca51fffc3507bbae35ebc8deed
benjaminsinaiko <ben.jamin@outlook.com>
2018-05-30 15:11:01 -0500

    Add dotenv-webpack, update webpack.config file

--- 9-Forkify/src/js/index.js
+++ 9-Forkify/src/js/index.js
@@ -1,6 +1,14 @@
 import axios from 'axios'
 
+// require('dotenv').config()
+
 async function getResults(query) {
-  axios()
+  const proxy = 'https://cors-anywhere.herokuapp.com/'
+  const key = process.env.FOOD_API_KEY
+  console.log('API Key: ', key)
+  // const res = await axios(
+  //   `${proxy}http://food2fork.com/api/search?key=${key}&q=${query}`
+  // )
+  // console.log(res)
 }
-getResults()
+getResults('pizza')
--- 9-Forkify/webpack.config.js
+++ 9-Forkify/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const Dotenv = require('dotenv-webpack')
 
 module.exports = {
   entry: ['babel-polyfill', './src/js/index.js'],
@@ -14,15 +15,21 @@ module.exports = {
     new HtmlWebpackPlugin({
       filename: 'index.html',
       template: './src/index.html'
-    })
+    }),
+    new Dotenv()
   ],
+  node: {
+    fs: 'empty'
+  },
   module: {
-    rules: [{
-      test: /\.js$/,
-      exclude: /node_modules/,
-      use: {
-        loader: 'babel-loader'
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
       }
-    }]
+    ]
   }
 }
