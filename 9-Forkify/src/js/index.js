26f440b1ffe41663620db353ac967ebdaae4e3c3
benjaminsinaiko <ben.jamin@outlook.com>
2018-05-30 15:20:50 -0500

    Sample API call working, index.js

--- 9-Forkify/src/js/index.js
+++ 9-Forkify/src/js/index.js
@@ -5,10 +5,14 @@ import axios from 'axios'
 async function getResults(query) {
   const proxy = 'https://cors-anywhere.herokuapp.com/'
   const key = process.env.FOOD_API_KEY
-  console.log('API Key: ', key)
-  // const res = await axios(
-  //   `${proxy}http://food2fork.com/api/search?key=${key}&q=${query}`
-  // )
-  // console.log(res)
+  try {
+    const res = await axios(
+      `${proxy}http://food2fork.com/api/search?key=${key}&q=${query}`
+    )
+    const recipes = res.data
+    console.log(recipes)
+  } catch (error) {
+    console.log('Error: ', error)
+  }
 }
 getResults('pizza')
