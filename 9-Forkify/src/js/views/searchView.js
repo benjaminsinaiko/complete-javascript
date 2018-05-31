d65daedbbef5906e14383f57bdec9ae75cf8efa9
benjaminsinaiko <ben.jamin@outlook.com>
2018-05-31 00:37:55 -0500

    Add method to limit recipe titles in results display

--- 9-Forkify/src/js/views/searchView.js
+++ 9-Forkify/src/js/views/searchView.js
@@ -10,20 +10,36 @@ export const clearResults = () => {
   elements.searchResList.innerHTML = ''
 }
 
+const limitRecipeTitle = (title, limit = 17) => {
+  const newTitle = []
+  if (title.length > limit) {
+    title.split(' ').reduce((acc, cur) => {
+      if (acc + cur.length <= limit) {
+        newTitle.push(cur)
+      }
+      return acc + cur.length
+    }, 0)
+
+    // return the result
+    return `${newTitle.join(' ')} ...`
+  }
+  return title
+}
+
 const renderRecipe = recipe => {
   const markup = `
-      <li>
-        <a class="results__link results__link--active" href="#${
-          recipe.recipe_id
-        }">
-            <figure class="results__fig">
-                <img src="${recipe.image_url}" alt="${recipe.title}">
-            </figure>
-            <div class="results__data">
-                <h4 class="results__name">${recipe.title}</h4>
-                <p class="results__author">${recipe.publisher}</p>
-            </div>
-        </a>
+    <li>
+      <a class="results__link results__link--active" href="#${
+        recipe.recipe_id
+      }">
+          <figure class="results__fig">
+              <img src="${recipe.image_url}" alt="${recipe.title}">
+          </figure>
+          <div class="results__data">
+              <h4 class="results__name">${limitRecipeTitle(recipe.title)}</h4>
+              <p class="results__author">${recipe.publisher}</p>
+          </div>
+      </a>
     </li>
   `
   elements.searchResList.insertAdjacentHTML('beforeend', markup)
