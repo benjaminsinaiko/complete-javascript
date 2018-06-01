dd11a57a27e5b7b4768a13f799ac36c1eddd2286
benjaminsinaiko <ben.jamin@outlook.com>
2018-06-01 15:30:31 -0500

    Build recipes controller

--- 9-Forkify/src/js/models/Recipe.js
+++ 9-Forkify/src/js/models/Recipe.js
@@ -18,6 +18,7 @@ export default class Recipe {
       this.ingredients = res.data.recipe.ingredients
     } catch (error) {
       console.log(error)
+      alert('Something went wrong :(')
     }
   }
 
